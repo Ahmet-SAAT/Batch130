c2d6431dfa1dc5e516cc80839e9f646a5e07b67b
Ahmet-SAAT <122081152+Ahmet-SAAT@users.noreply.github.com>
2023-02-18 12:24:57 +0300

    18.02.2023

--- src/proje/BookStore/MiniBookStore.java
+++ src/proje/BookStore/MiniBookStore.java
@@ -1,5 +1,21 @@
 package proje.BookStore;
+/*
+Proje: Mini Book Store
+        Online bir kitap market için ürün yönetim uygulaması yapınız.
+        Kitap markette kitap ve defter satışı olacak, ancak ileride yeni ürün çeşidi eklenebilir olmalı.
 
+        Kitap özellikleri: id, isim, birim fiyat, stok, yazar adı, yayınevi,isbn no
+        Defter özellikleri: id, isim, birim fiyat, stok, marka, yaprak sayısı,ürün kodu
+
+        Kullanıcı ilgili kategorideki ürünleri listeleyebilmeli
+        Kullanıcı kategoriye göre ürün ekleyebilmeli,ürün mevcutsa uyarı verilmeli
+        Kullanıcı ürünleri benzersiz numaralarına(id) göre silebilmeli.
+        Kullanıcı ürünleri marka(defter)  veya yayınevine(kitap) göre filtreleyip listeleyebilmeli.
+
+        NotebookService:ÖDEV
+        updateProduct:opsitonel ödev
+
+        */
 import java.util.Scanner;
 
 public class MiniBookStore {
